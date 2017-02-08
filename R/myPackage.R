bdd9b4571837c2e715cfe470889f6aa273b629e0
Pablo Maldonado <pablo.maldonado@actum.cz>
2017-02-08 13:06:15 +0100

    here we go

---
+++ R/myPackage.R
@@ -0,0 +1,36 @@
+#' Sample skeleton for custom science - Main application function
+#'
+#' @import keboola.r.docker.application
+#' @export
+#' @param datadir Path to data directory.
+doSomething <- function(datadir) {
+  # read input
+# Get the file from FTP
+
+		# do something
+	app <- DockerApplication$new(datadir)
+	app$readConfig()
+
+	UserName <- app$getParameters()$UserName
+	PassWord <- app$getParameters()$PassWord
+
+	
+	url <- "sftp://transfer6.silverpop.com/download/"
+	userpwd <- paste0(UserName,":",PassWord)
+	
+	last_file_df <- read.csv("in/tables/last_file.csv")
+	last_file <- last_file_df[1,1]
+	
+
+	
+	zipfile <- getBinaryURL(paste(url,last_file,sep=""), userpwd = userpwd)
+	writeBin(zipfile, "test.zip")
+	unzip("test.zip")
+
+	csv_file <- gsub("zip","csv",last_file)
+	
+	
+	df <- read.csv(csv_file)
+	
+	write.csv(df, file = file.path(datadir, "out/tables/events.csv"), row.names = FALSE)
+}
