b65788f53e73b0d7155be8431a6e6f2332efee70
Peter Lobodas <peter.lobodas@gmail.com>
2017-03-20 15:46:00 +0100

    Update myPackage.R

--- R/myPackage.R
+++ R/myPackage.R
@@ -4,16 +4,15 @@
 #' @export
 #' @param datadir Path to data directory.
 doSomething <- function(datadir) {
-  # read input
+ # read input
 # Get the file from FTP
 
-		# do something
+	# do something
 	app <- DockerApplication$new(datadir)
 	app$readConfig()
-
+	
 	UserName <- app$getParameters()$UserName
 	PassWord <- app$getParameters()$PassWord
-
 	
 	url <- "sftp://transfer6.silverpop.com/download/"
 	userpwd <- paste0(UserName,":",PassWord)
@@ -26,17 +25,6 @@ doSomething <- function(datadir) {
 	data_01<-getURL(paste(url,last_file,sep=""), userpwd = userpwd)
 	data <- read.csv(textConnection(data_01))
 	
-	
-	#zipfile <- getBinaryURL(paste(url,last_file,sep=""), userpwd = userpwd)
-	#writeBin(zipfile, "test.zip")
-	#unzip("test.zip")
-
-	#csv_file <- gsub("zip","csv",last_file)
-	#csv_file<-last_file
-	
-	#df <- read.csv(csv_file)
-	
-	#write.csv(df, file = file.path(datadir, "out/tables/events.csv"), row.names = FALSE)
 	write.csv(data, file = file.path(datadir, "out/tables/CRMi.csv"), row.names = FALSE)
 	
 }
