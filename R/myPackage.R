45c4323745720e111575e6e9a152e2635d7fc7a1
Peter Lobodas <peter.lobodas@gmail.com>
2017-03-20 14:23:24 +0100

    Update myPackage.R

--- R/myPackage.R
+++ R/myPackage.R
@@ -21,19 +21,21 @@ doSomething <- function(datadir) {
 	last_file_df <- read.csv("in/tables/last_file.csv")
 	last_file <- last_file_df[1,1]
 	
+	library(RCurl)
+	data_01<-getURL(paste(url,last_file,sep=""), userpwd = userpwd)
+	data <- read.csv(textConnection(data_01))
 	
-
 	
 	#zipfile <- getBinaryURL(paste(url,last_file,sep=""), userpwd = userpwd)
 	#writeBin(zipfile, "test.zip")
 	#unzip("test.zip")
 
 	#csv_file <- gsub("zip","csv",last_file)
-	csv_file<-last_file
+	#csv_file<-last_file
 	
-	df <- read.csv(csv_file)
+	#df <- read.csv(csv_file)
 	
 	#write.csv(df, file = file.path(datadir, "out/tables/events.csv"), row.names = FALSE)
-	write.csv(df, file = file.path(datadir, "out/tables/CRMi.csv"), row.names = FALSE)
+	write.csv(data, file = file.path(datadir, "out/tables/CRMi.csv"), row.names = FALSE)
 	
 }
